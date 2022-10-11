2122b2478f5cacd6e9c1856d0524cea25c8b5279
abondar24 <abondar1992@gmail.com>
2022-10-11 17:09:27 +0200

    register fix

--- contacts/src/context/actions/auth/register.js
+++ contacts/src/context/actions/auth/register.js
@@ -1,4 +1,4 @@
-import { REGISTER_FAIL, REGISTER_LOADING, REGISTER_SUCCESS } from "../../../constants/actionTypes";
+import { REGISTER_FAIL, REGISTER_LOADING, REGISTER_SUCCESS, CLEAR_AUTH_STATE } from "../../../constants/actionTypes";
 import axiosInstance from "../../../helpers/axiosInterceptor"
 
 export default ({ email, password, userName: username, firstName: first_name, lastName: last_name }) => dispatch => {
