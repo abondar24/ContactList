d9c38bea23487702cf06ad97c134843b00316b3e
abondar24 <abondar1992@gmail.com>
2022-11-01 10:52:27 +0100

    fix upload image

--- contacts/src/helpers/uploadImage.js
+++ contacts/src/helpers/uploadImage.js
@@ -1,16 +1,13 @@
 import storage from '@react-native-firebase/storage'
 
 export default (file) => (onSuccess) => (onError) => {
-    const path = 'contact-picutres/user/555/' + file.creationDate || file.modificationDate;
 
+    const path = 'contact-picutres/user/555/' + file.modificationDate || file.creationDate || file.path;
     const ref = storage().ref(path);
-
     const task = ref.putFile(file.path);
-
 
     task.then(async () => {
         const url = await ref.getDownloadURL();
-
         onSuccess(url);
     }).then(error => {
         onError(error)
